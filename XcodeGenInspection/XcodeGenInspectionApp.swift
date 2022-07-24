467092ef9d7ca35529b90f1d5616570ecd900032
杉岡成哉 <a1209s518n928@gmail.com>
2022-07-24 17:26:23 +0900

    Initial Commit

---
+++ XcodeGenInspection/XcodeGenInspectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGenInspectionApp.swift
+//  XcodeGenInspection
+//
+//  Created by 杉岡成哉 on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGenInspectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
